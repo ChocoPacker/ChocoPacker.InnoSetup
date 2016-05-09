817df51dad4e1232ee658b293a16b173ce692719
fenriv <fenriv@gmail.com>
2016-05-09 23:15:31 +0200

    Reconfiguration of project infrastructure

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
-﻿namespace ChocoPacker.InnoSetup
-{
+﻿using System;
+
+namespace ChocoPacker.InnoSetup
+{    
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Choco Test");
         }
     }
 }
